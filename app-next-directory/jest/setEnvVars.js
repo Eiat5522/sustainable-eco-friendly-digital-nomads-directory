8b44f9e3be1af0fae8eda80be573cafc51cd4a30
Eiat5522 <YOUR_EMAILeiat52@gmail.com>
2025-07-10 09:58:06 +0700

    fix: update Jest command in settings and add dotenv dependency; create jsconfig archive

--- app-next-directory/jest/setEnvVars.js
+++ app-next-directory/jest/setEnvVars.js
@@ -1,2 +1,3 @@
 // jest/setEnvVars.js
 require('dotenv').config({ path: require('path').resolve(__dirname, '../.env.test') });
+console.log('Loaded .env.test:', process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, process.env.NODE_ENV); // DEBUG: Remove after troubleshooting
