82c9cf777b0333bdc9888e0c68e6158d686ce1b3
Eiat5522 <eiat52@gmail.com>
2025-09-15 20:21:39 +0700

    chore: add @types/validator@13.15.3 to dependencies in pnpm-lock.yaml

--- app-next-directory/src/models/User.ts
+++ app-next-directory/src/models/User.ts
@@ -1,5 +1,5 @@
 import mongoose, { Document, Schema } from 'mongoose';
-import validator from 'validator';
+import isEmail from 'validator/lib/isEmail';
 
 /**
  * User Model - Index Management Notes:
