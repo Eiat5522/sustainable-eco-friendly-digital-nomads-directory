8cdff7f0ac1bce6e5f141731dded4e629dadb209
Eiat5522 <132329283+Eiat5522@users.noreply.github.com>
2025-11-06 04:24:01 +0700

    Update setup.sh
    Temporary removed playwright dev dependencies.

--- setup.sh
+++ setup.sh
@@ -10,14 +10,14 @@ echo "Installing pnpm dependencies for all workspaces..."
 pnpm install
 
 # Step 2: Install Playwright browsers
-echo "Installing Playwright browsers..."
-pnpm playwright install --with-deps
+#echo "Installing Playwright browsers..."
+#pnpm playwright install --with-deps
 
-# Step 3: Extract Sanity Schemas to schema.json
+# Step 2: Extract Sanity Schemas to schema.json
 echo "Extracting Sanity's Schemas"
 npx -y sanity@latest schema extract
 
-# Step 4: Generate TypeScript types for Sanity schemas
+# Step 3: Generate TypeScript types for Sanity schemas
 echo "Generating TypeScript types for Sanity schemas..."
 npx -y sanity@latest typegen generate
 
