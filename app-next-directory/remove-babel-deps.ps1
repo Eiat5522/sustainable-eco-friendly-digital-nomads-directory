32ebce66e860ab0fbf7a687346e04f3b4016847a
Eiat5522 <eiat52@gmail.com>
2025-11-21 14:17:42 +0700

    cleanup: outdated documentation and .ps1 scripts

--- app-next-directory/remove-babel-deps.ps1
+++
@@ -1,9 +0,0 @@
-# PowerShell script to remove Babel dependencies since we're using SWC
-
-Write-Host "Removing Babel dependencies..." -ForegroundColor Green
-
-# Remove Babel-related dependencies
-pnpm remove @babel/core @babel/preset-env @babel/preset-react @babel/preset-typescript babel-jest
-
-Write-Host "Babel dependencies removed successfully!" -ForegroundColor Green
-Write-Host "The project is now using SWC for both Next.js compilation and Jest testing." -ForegroundColor Cyan
